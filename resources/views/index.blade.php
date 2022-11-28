022b8ff79d819fc2dd1da6e5885fbb42e243e88a
hanisrafiqah <93245167+hanisrafiqah@users.noreply.github.com>
2022-11-28 17:06:33 +0800

    new update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,12 +6,12 @@
             <h1 class="headline">MentalCare <br/> <span class="home_text">System</h1>
             <h5 class="para para-light py-3">Talk to a Therapist from the comfort of your home</h5>
             <div class="d-flex align-items-center">
-                <p class="p-2"><i class="fas fa-laptop-house fa-lg"></i></p>
+                <p class="p-2"><i class="fas fa-phone fa-lg"></i></p>
                 <p>Choose your preferred mode of Counselling.</p>  
             </div>
             <div class="d-flex align-items-center">
-                <p class="p-2"><i class="fas fa-wifi fa-lg"></i></p>
-                <p>Chat or make a video call with your therapist.</p>  
+                <p class="p-2"><i class="fas fa-clock fa-lg"></i></p>
+                <p>Chat or make a video call with your therapist anytime you want.</p>  
             </div>
             <div class="my-3 d-flex justify-content-start">
                 <a class="btn mx-3" href="register">Get Started</a>
