2e7643558855e5069182e34d1a97ab3fbddd956a
Rohaizaazira <118188847+Rohaizaazira@users.noreply.github.com>
2022-11-30 04:56:27 +0800

    Update my_appoinment.blade.php

--- resources/views/my_appoinment.blade.php
+++ resources/views/my_appoinment.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.app')
 @section('content')
 <section class="dashboard d-flex py-5">
+<style>
+    th{
+        background-color: black;
+    }
+td,th,tr
+{border-left:1px solid white;
+border-top:1px solid white;}
+table
+{border-right:1px solid white;
+border-bottom:1px solid white;
+align-self: auto;
+text-align: center;}
+</style>
+    <html>
+        <body>
     <div class="container mt-4">
         <div class="row mt-5">
 
             <div align="center">
-                <table>
-                    <tr style="background-color:black;">
+            <table style="width: 100%;">
+                    <tr >
                         <th style="padding:10px;font: size 20px;color:white;">Counsellor Name</th>
                         <th style="padding:10px;font: size 20px;color:white;">Date</th>
-                        <th style="padding:10px;font: size 20px;color:white;">time</th>
+                        <th style="padding:10px;font: size 20px;color:white;">Time</th>
                         <th style="padding:10px;font: size 20px;color:white;">Action</th>
                     </tr>
                     <tr>
                         <td style="color: white;">Dr Amalina</td>
                         <td style="color: white;">23/12/2022</td>
                         <td style="color: white;">11.30am</td>
-                        <td style="color: white;"><button onclick="myreject()">Reject</button></td>
-                        <td style="color: white;"><button onclick="myFunction()">Approved</button></td>
+                        <td style="color: white;"><button onclick="myreject()">Reject</button>//<button onclick="myFunction()">Approved</button></td>
+                        
                     </tr>
                     <script>
                         function myFunction() {
@@ -34,6 +49,8 @@
             </div>
         </div>
     </div>
+                    </body>
+    </html>
     </section>
 @endsection
 @section('scripts')
